ff287011a9e76acfc36031789f55dac71eeed2af
evlaa1990 <127751286+evangoulden-emis@users.noreply.github.com>
2025-05-15 15:39:59 +0100

    Added table output for various BGP functions

--- network_tool.py
+++ network_tool.py
@@ -8,8 +8,9 @@ import ipaddress
 
 BGPVIEW = "https://api.bgpview.io"
 
+
 def main(args: Namespace):
-    if args.fqdn is None and args.nameserver is None and args.subnet is None and args.target_host is None\
+    if args.fqdn is None and args.nameserver is None and args.subnet is None and args.target_host is None \
             and args.bgp is None:
         print("No arguments provided. Use --help for more information.")
         return
@@ -22,11 +23,12 @@ def main(args: Namespace):
 
 
 def bgp(bgp: Namespace):
+    asn = None
+    console = Console()
     try:
         if bgp.isdigit():
             '''Treat BGP as an ASN'''
-            asn = int(bgp)
-            console = Console()
+            asn = int(str(bgp))
             console.print(f"[bold]BGP ASN:[/bold] {asn}")
             # Perform BGP lookup using the ASN
             try:
@@ -43,27 +45,79 @@ def bgp(bgp: Namespace):
                     table.add_column("Status", style="yellow")
                     for prefix in prefixes:
                         table.add_row(
-                        prefix['name'],
-                                      prefix['prefix'],
-                                      str(prefix['cidr']),
-                                      "✓"
-                                      )
+                            prefix['name'],
+                            prefix['prefix'],
+                            str(prefix['cidr']),
+                            "✓"
+                        )
                     console.print(table, new_line_start=True)
             except requests.exceptions.RequestException as e:
                 console.print(f"[bold red]Error:[/bold red] {e}")
                 return
+        elif get_ip_or_none(str(bgp)):
+            prefix = ipaddress.ip_network(str(bgp))
+            try:
+                response = requests.get(f"{BGPVIEW}/prefix/{prefix}")
+                if response.status_code == 200:
+                    data = response.json()
+                    # Extract relevant information
+                    prefix_info = data.get('data', {})
+                    prefixes = prefix_info.get('ipv4_prefixes', [])
+                    prefix_table = Table(title=f"Prefix {prefix} Information", show_lines=True)
+                    prefix_table.add_column("Name", style="cyan")
+                    prefix_table.add_column("Prefix", style="green")
+                    prefix_table.add_column("ASN", style="magenta")
+                    prefix_table.add_column("Status", style="yellow")
+                    prefix_table.add_row(
+                        prefix_info['name'],
+                        prefix_info['prefix'],
+                        str(prefix_info['cidr']),
+                        "✓"
+                    )
+                    asn_count = len(prefix_info['asns'])
+                    asn_table = Table(title=f"ASN Information for {prefix}", show_lines=True)
+                    asn_table.add_column("ASN", style="cyan")
+                    asn_table.add_column("Name", style="green")
+                    asn_table.add_column("Description", style="magenta")
+                    asn_table.add_column("Country Code", style="yellow")
+                    asn_table.add_column("Upstream ASNs", style="blue")
+                    asn_table.add_column("Upstream ASN Names", style="cyan")
+                    for asn in prefix_info['asns']:
+                        for upstream_asn in asn['prefix_upstreams']:
+                            asn_table.add_row(
+                                str(asn['asn']),
+                                asn['name'],
+                                asn['description'],
+                                asn['country_code'],
+                                str(upstream_asn['asn']),
+                                upstream_asn['name']
+                            )
+
+                    console.print(prefix_table, new_line_start=True)
+                    console.print(asn_table, new_line_start=True)
+            except requests.exceptions.RequestException as e:
+                console.print(f"[bold red]Error:[/bold red] {e}")
+                return
+
         else:
             print(f"Error: Unable to retrieve BGP information for ASN {asn}.")
+    except ValueError:
+        print(f"The value passed to --bgp is invalid: {str(bgp)}.")
+        print(f"Make sure the value passed is a valid ASN number of a valid IP Address/Subnet")
+        return
 
-    except NameError:
-        pass
 
+def get_ip_or_none(ip_str):
+    try:
+        return ipaddress.ip_network(ip_str)
+    except ValueError:
+        return None
 
 
 def fqdn(_fqdn: str, nameserver: str = None):
-    rdtypes = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'PTR', 'SRV', 'TXT', 'CAA', 'DS', 'DNSKEY', 'RRSIG', 'NSEC', 'NSEC3', 'NSEC3PARAM']
+    rdtypes = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'PTR', 'SRV', 'TXT', 'CAA', 'DS', 'DNSKEY', 'RRSIG', 'NSEC',
+               'NSEC3', 'NSEC3PARAM']
     console = Console()
-
 
     system_nameservers = []
     try:
@@ -75,10 +129,13 @@ def fqdn(_fqdn: str, nameserver: str = None):
         else:
             resolver.default_resolver = resolver.Resolver()
             system_nameservers = resolver.default_resolver.nameservers
-            console.print(f"[bold]FQDN:[/bold] {_fqdn}, [bold]Nameserver:[/bold] {system_nameservers[0] if system_nameservers else 'unknown'}")
+            console.print(
+                f"[bold]FQDN:[/bold] {_fqdn}, [bold]Nameserver:[/bold] {system_nameservers[0] if system_nameservers else 'unknown'}")
 
         # Create a table
-        table = Table(title=f"DNS Records for {_fqdn} using nameserver: {nameserver if nameserver else system_nameservers[0]}", show_lines=True)
+        table = Table(
+            title=f"DNS Records for {_fqdn} using nameserver: {nameserver if nameserver else system_nameservers[0]}",
+            show_lines=True)
         table.add_column("Record Type", style="cyan")
         table.add_column("Data", style="green")
         table.add_column("Status", style="yellow")
@@ -100,7 +157,6 @@ def fqdn(_fqdn: str, nameserver: str = None):
     except Exception as e:
         console.print(f"[bold red]Error:[/bold red] {e}")
         return
-
 
 
 if __name__ == "__main__":
